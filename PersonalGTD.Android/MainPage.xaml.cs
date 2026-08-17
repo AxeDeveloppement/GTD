03cdd65fcd5797f0ccd29e830b67a682508de219
AxeDeveloppement <axeldeveloppement@users.noreply.github.com>
2026-08-17 03:41:48 +0200

    android correction

--- PersonalGTD.Android/MainPage.xaml.cs
+++ PersonalGTD.Android/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.WebView.Maui;
+using PersonalGTD.Android.Components;
 
 namespace PersonalGTD.Android;
 
@@ -6,13 +7,12 @@ public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
-	       InitializeComponent();
+		InitializeComponent();
 
-	       blazorWebView.RootComponents.Add(new Microsoft.AspNetCore.Components.WebView.Maui.RootComponent
-	       {
-	           Selector = "#app",
-	           ComponentType = typeof(Components.Routes)
-	       });
+		// Configuration programmatique du composant racine Blazor.
+		// L'attribut XAML RootComponent n'existe pas sur BlazorWebView dans MAUI 10.
+		// Il faut utiliser la collection RootComponents pour lier le composant au div #app de wwwroot/index.html.
+		blazorWebView.RootComponents.Add(new RootComponent { Selector = "#app", ComponentType = typeof(Routes) });
 	}
 }
 
